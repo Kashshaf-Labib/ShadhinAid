745bab79d1ebeed680b1cf804f66cf483929d0c3
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2024-08-16 11:20:35 +0600

    Updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,30 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { Toaster } from "react-hot-toast";
-import "./globals.css";
-import Head from "next/head";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import { Toaster } from "react-hot-toast"
+import "./globals.css"
+import Head from "next/head"
+import Navbar from "./components/Navbar"
 
 export const metadata: Metadata = {
   title: "Shadhin Aid",
   description: "A fundraising platform for the underprivileged",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
       <body>
+        <Navbar />
         {children}
-        <Toaster/>
+        <Toaster />
       </body>
     </html>
-  );
+  )
 }
